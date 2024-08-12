4f729f0f6f190f9bc74d3eb6e1227c67b37d1793
gludovatza <attila@gludovatz.hu>
2024-08-12 18:39:21 +0200

    Company Model & Migration file

---
+++ app/Models/Company.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Spatie\Permission\Models\Role;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Company extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name', 'slug'];
+
+    public function members(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class);
+    }
+
+    // public function companyLocations(): HasMany
+    // {
+    //     return $this->hasMany(Location::class);
+    // }
+
+    public function roles(): HasMany
+    {
+        return $this->hasMany(Role::class);
+    }
+}
---
+++ database/migrations/2024_08_12_180236_create_companies_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug');
+            $table->timestamps();
+        });
+
+        Schema::create('company_user', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('company_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
+            $table->timestamps();
+
+            $table->unique(['company_id', 'user_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('company_user');
+        Schema::dropIfExists('companies');
+    }
+};
