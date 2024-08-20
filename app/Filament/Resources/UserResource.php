401181248b4ac80b31759e5fb22b53769f3e8ae8
gludovatza <attila@gludovatz.hu>
2024-08-20 17:57:50 +0200

    UserResource: typo fix

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -60,7 +60,7 @@ class UserResource extends Resource
                     Forms\Components\TextInput::make('password')
                         ->password()
                         ->maxLength(255)
-                        ->required(static fn(Page $livewire): string => $livewire instanceof CreateUser,)
+                        ->required(static fn(Page $livewire): string => $livewire instanceof CreateUser)
                         ->dehydrateStateUsing(
                             fn(?string $state): ?string =>
                             filled($state) ? Hash::make($state) : null
