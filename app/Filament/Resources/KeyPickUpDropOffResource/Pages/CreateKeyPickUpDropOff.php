2c07a9c8d7fff4d8873c381aa3cd770752984fd7
gludovatza <attila@gludovatz.hu>
2024-08-21 23:08:05 +0200

    CreateKeyPickUpDropOff: disabled items->init, new create button label, hide "can create another" button

---
+++ app/Filament/Resources/KeyPickUpDropOffResource/Pages/CreateKeyPickUpDropOff.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Filament\Resources\KeyPickUpDropOffResource\Pages;
+
+use Filament\Actions;
+use Filament\Facades\Filament;
+use Filament\Actions\Action;
+use Filament\Resources\Pages\CreateRecord;
+use App\Filament\Resources\KeyPickUpDropOffResource;
+
+class CreateKeyPickUpDropOff extends CreateRecord
+{
+    protected static string $resource = KeyPickUpDropOffResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        if(!isset($data['company_id']))
+            $data['company_id'] = Filament::getTenant()->id;
+
+        if(!isset($data['pick_up_user_id']))
+            $data['pick_up_user_id'] = auth()->id();
+
+        return $data;
+    }
+
+    protected function getCreateFormAction(): Action
+    {
+        return Action::make('create')
+            ->label(__('module_names.key_mgmt.pick_up'))
+            ->submit('create')
+            ->keyBindings(['mod+s']);
+    }
+
+    public static function canCreateAnother(): bool
+    {
+        return false;
+    }
+}
